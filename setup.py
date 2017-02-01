f8fe73136d432587529c2d17fbb5e3093531197b
Sebastien Duthil <dev.wazo+sduthil@gmail.com>
2017-02-01 14:17:36 -0500

    add wazo_uuid python package

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright 2017 The Wazo Authors  (see AUTHORS file)
+# SPDX-License-Identifier: GPL-3.0+
+
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name='wazo_uuid',
+    version='1.0',
+    description='Python code related to Wazo UUID',
+    author='Wazo Authors',
+    author_email='dev@wazo.community',
+    url='http://wazo.community',
+    license='GPLv3',
+    packages=find_packages(),
+    scripts=[
+        'bin/xivo-configure-uuid'
+    ],
+)
---
+++ wazo_uuid/uuid_.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 The Wazo Authors  (see AUTHORS file)
+# SPDX-License-Identifier: GPL-3.0+
+
+import logging
+import os
+import re
+import subprocess
+import uuid
+
+logger = logging.getLogger(__name__)
+XIVO_UUID_FILENAME = '/etc/profile.d/xivo_uuid.sh'
+XIVO_UUID_RE = re.compile(r'(export )?XIVO_UUID=([0-9a-f-]{36})$')
+
+
+def get_wazo_uuid():
+    for uuid_unchecked in _find_uuid():
+        if not uuid_unchecked:
+            continue
+
+        try:
+            uuid_checked = str(uuid.UUID(uuid_unchecked))
+        except ValueError:
+            logger.debug('ignoring invalid Wazo UUID "%s"', uuid_unchecked)
+            continue
+
+        return uuid_checked
+
+    raise Exception('XIVO_UUID not found')
+
+
+def _find_uuid():
+    yield _find_uuid_environ()
+    yield _find_uuid_systemctl()
+    yield _find_uuid_file()
+
+
+def _find_uuid_environ():
+    return os.environ.get('XIVO_UUID')
+
+
+def _find_uuid_systemctl():
+    try:
+        environment_string = subprocess.check_output(['systemctl', 'show-environment'])
+    except subprocess.CalledProcessError:
+        return None
+
+    return _extract_uuid_env_variable(environment_string)
+
+
+def _find_uuid_file():
+    try:
+        with open(XIVO_UUID_FILENAME, 'r') as f:
+            environment_string = f.read()
+    except IOError:
+        return None
+
+    return _extract_uuid_env_variable(environment_string)
+
+
+def _extract_uuid_env_variable(environment_string):
+    for line in environment_string.split('\n'):
+        match = XIVO_UUID_RE.match(line)
+        if match:
+            return match.group(2)
+    return None
